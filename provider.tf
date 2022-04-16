82e72d3a35cfa035ff0a330af4924e348066be91
frank <frank@yahoo.com>
2022-04-16 13:29:15 +0100

    edited provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-2"
-  access_key = "AKIA6LAQVREDPNHZTL3J"
-  secret_key = "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"
+  access_key = "xxxxxxAKIA63J"
+  secret_key = "xxxxxvt8a5yzNJyY"
 }
